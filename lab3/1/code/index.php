c88b4c27522773736ca9ab94252fb692168fef9a
chervyachiha <113230399+chervyachiha@users.noreply.github.com>
2024-03-30 19:55:58 +0200

    Add files via upload

--- lab3/1/code/index.php
+++ lab3/1/code/index.php
@@ -1,1 +1,19 @@
+<?php
+//a
+$str = 'ahb acb aeb aeeb adcb axeb';
+$regexp='/a[a-z]{2}b/ui';
+$matches = array();
+$count = preg_match_all($regexp, $str, $matches);
+echo "Найдено строк: {$count}\n";
+var_dump($matches);
 
+//b
+$text = 'a1b2c3';
+$change = '/[0-9]/ui';
+function replaceWithCube($matches) {
+    $number = (int)$matches[0];
+    $cubedNumber = pow($number, 3);
+    return $cubedNumber;
+}
+$result = preg_replace_callback($change, 'replaceWithCube', $text);
+echo $result;
